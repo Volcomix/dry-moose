fa799e2231e277a25970eb531a7033a396b52883
Volcomix <volcomix@gmail.com>
2015-09-17 20:47:01 +0200

    Add MACD wrapper

--- addon/ta-lib.cc
+++ addon/ta-lib.cc
@@ -6,6 +6,22 @@
 using node::AtExit;
 using namespace v8;
 
+double* v8ToDoubleArray(Local<Array> arr, int length) {
+    double* result = new double[length];
+    for (int i = 0; i < length; i++) {
+        result[i] = arr->Get(i)->NumberValue();
+    }
+    return result;
+}
+
+Local<Array> doubleArrayToV8(Isolate* isolate, double* arr, int length) {
+    Local<Array> result = Array::New(isolate, length);
+    for (int i = 0; i < length; i++) {
+        result->Set(i, Number::New(isolate, arr[i]));
+    }
+    return result;
+}
+
 /*
  * TA_SMA - Simple Moving Average
  * 
@@ -18,25 +34,22 @@ using namespace v8;
  *    Number of period
  */
 void SMA(const FunctionCallbackInfo<Value>& args) {
-    TA_RetCode retCode;
-    
     Isolate* isolate = args.GetIsolate();
     
     // Get input
     int startIdx = args[0]->NumberValue();
     int endIdx = args[1]->NumberValue();
     Local<Array> inArr = Local<Array>::Cast(args[2]);
-    int length = inArr->Length();
     int optInTimePeriod = args[3]->NumberValue();
     
-    double * inReal = new double[length];
-    for (int i = 0; i < length; i++) {
-        inReal[i] = inArr->Get(i)->NumberValue();
-    }
+    int length = inArr->Length();
+    double* inReal = v8ToDoubleArray(inArr, length);
     
     // Prepare output
+    TA_RetCode retCode;
     int outBegIdx;
     int outNBElement;
+    
     double * outReal = new double[length];
     
     // Call TA-Lib function
@@ -46,10 +59,7 @@ void SMA(const FunctionCallbackInfo<Value>& args) {
     delete[] inReal;
     
     // Get output
-    Local<Array> outArr = Array::New(isolate, outNBElement);
-    for (int i = 0; i < outNBElement; i++) {
-        outArr->Set(i, Number::New(isolate, outReal[i]));
-    }
+    Local<Array> outArr = doubleArrayToV8(isolate, outReal, outNBElement);
     
     delete[] outReal;
     
@@ -63,19 +73,83 @@ void SMA(const FunctionCallbackInfo<Value>& args) {
     args.GetReturnValue().Set(obj);
 }
 
+/*
+ * TA_MACD - Moving Average Convergence/Divergence
+ * 
+ * Input  = double
+ * Output = double, double, double
+ * 
+ * Optional Parameters
+ * -------------------
+ * optInFastPeriod:(From 2 to 100000)
+ *    Number of period for the fast MA
+ * 
+ * optInSlowPeriod:(From 2 to 100000)
+ *    Number of period for the slow MA
+ * 
+ * optInSignalPeriod:(From 1 to 100000)
+ *    Smoothing for the signal line (nb of period)
+ */
+void MACD(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+    
+    // Get input
+    int startIdx = args[0]->NumberValue();
+    int endIdx = args[1]->NumberValue();
+    Local<Array> inArr = Local<Array>::Cast(args[2]);
+    int optInFastPeriod = args[3]->NumberValue();
+    int optInSlowPeriod = args[4]->NumberValue();
+    int optInSignalPeriod = args[5]->NumberValue();
+    
+    int length = inArr->Length();
+    double* inReal = v8ToDoubleArray(inArr, length);
+    
+    // Prepare output
+    TA_RetCode retCode;
+    int outBegIdx;
+    int outNBElement;
+    
+    double * outMACD = new double[length];
+    double * outMACDSignal = new double[length];
+    double * outMACDHist = new double[length];
+    
+    // Call TA-Lib function
+    retCode = TA_MACD(startIdx, endIdx, inReal,
+                      optInFastPeriod, optInSlowPeriod, optInSignalPeriod,
+                      &outBegIdx, &outNBElement, outMACD, outMACDSignal, outMACDHist);
+
+    delete[] inReal;
+    
+    // Get output
+    Local<Array> v8MACD = doubleArrayToV8(isolate, outMACD, outNBElement);
+    Local<Array> v8MACDSignal = doubleArrayToV8(isolate, outMACDSignal, outNBElement);
+    Local<Array> v8MACDHist = doubleArrayToV8(isolate, outMACDHist, outNBElement);
+    
+    delete[] outMACD;
+    delete[] outMACDSignal;
+    delete[] outMACDHist;
+    
+    // Set up result
+    Local<Object> obj = Object::New(isolate);
+    obj->Set(String::NewFromUtf8(isolate, "retCode"), Number::New(isolate, retCode));
+    obj->Set(String::NewFromUtf8(isolate, "outBegIdx"), Number::New(isolate, outBegIdx));
+    obj->Set(String::NewFromUtf8(isolate, "outNBElement"), Number::New(isolate, outNBElement));
+    obj->Set(String::NewFromUtf8(isolate, "outMACD"), v8MACD);
+    obj->Set(String::NewFromUtf8(isolate, "outMACDSignal"), v8MACDSignal);
+    obj->Set(String::NewFromUtf8(isolate, "outMACDHist"), v8MACDHist);
+    
+    args.GetReturnValue().Set(obj);
+}
+
 static void Shutdown(void*) {
     TA_Shutdown();
-    // printf("TA-Lib did correctly shutdown.\n");
 }
 
 void init(Local<Object> exports) {
-    TA_RetCode retCode = TA_Initialize();    
-    if(retCode != TA_SUCCESS) {
-        // printf("Cannot initialize TA-Lib (%d)!\n", retCode);
-    } else {
-        // printf("TA-Lib correctly initialized.\n");
+    if(TA_Initialize() == TA_SUCCESS) {
         
         NODE_SET_METHOD(exports, "SMA", SMA);
+        NODE_SET_METHOD(exports, "MACD", MACD);
         
         AtExit(Shutdown);
     }
--- src/test/ta-lib.ts
+++ src/test/ta-lib.ts
@@ -17,4 +17,18 @@ describe('ta-lib', function() {
             result.outReal.should.have.members([2, 3, 4]);
         });
     });
+    describe('#MACD()', function() {
+        it('should return Moving Average Convergence/Divergence', function() {
+            var result = TA.MACD(0, 4, [1, 8, 18, 7, 11], 2, 3, 2);
+            result.retCode.should.equal(0);
+            result.outBegIdx.should.equal(3);
+            result.outNBElement.should.equal(2);
+            result.outMACD.should.have.length(2);
+            result.outMACD.should.include(1);
+            result.outMACDSignal.should.have.length(2);
+            result.outMACDSignal.should.include(2.5);
+            result.outMACDHist.should.have.length(2);
+            result.outMACDHist.should.include(-1.5);
+        });
+    });
 });
