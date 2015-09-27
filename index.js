ffa95a6007f9c96aca55835e7206613e5e6bef5b
Volcomix <volcomix@gmail.com>
2015-09-27 13:52:25 +0200

    Add sequential runs into sample index.js

--- index.js
+++ index.js
@@ -10,24 +10,33 @@ var ConsoleInvestor = require('./release/investors/ConsoleInvestor');
 var DemoCelebrator = require('./release/celebrators/DemoCelebrator');
 var DemoCapacitor = require('./release/capacitors/DemoCapacitor');
 
-new Supervisor(
-	new GenericASCIIM1(
-		path.join(
-			os.homedir(),
-			'Téléchargements',
-			'HISTDATA_COM_ASCII_EURUSD_M1201506',
-			'DAT_ASCII_EURUSD_M1_201506.csv'
-		),
-		[{
-			countdown: moment({ minutes: 10 }),
-			expiration: moment({ minutes: 30 }),
-			payout: 0.75
-		}]
-	),
-	new DummyProcessor(),
-	new ConsoleInvestor(),
-	new DemoCelebrator(),
-	new DemoCapacitor(100)
-)
+var baseDir = path.join(os.homedir(), 'Téléchargements');
+
+var rewards = [{
+	countdown: moment({ minutes: 10 }),
+	expiration: moment({ minutes: 30 }),
+	payout: 0.75
+}];
+
+var processor = new DummyProcessor();
+var investor = new ConsoleInvestor();
+var celebrator = new DemoCelebrator();
+var capacitor = new DemoCapacitor(100);
+
+new Supervisor(new GenericASCIIM1(path.join(baseDir,
+	'HISTDATA_COM_ASCII_EURUSD_M1201506/DAT_ASCII_EURUSD_M1_201506.csv'
+), rewards), processor, investor, celebrator, capacitor)
 .run()
+.then(function() {
+	return new Supervisor(new GenericASCIIM1(path.join(baseDir,
+		'HISTDATA_COM_ASCII_EURUSD_M1201507/DAT_ASCII_EURUSD_M1_201507.csv'
+	), rewards), processor, investor, celebrator, capacitor)
+	.run()
+})
+.then(function() {
+	return new Supervisor(new GenericASCIIM1(path.join(baseDir,
+		'HISTDATA_COM_ASCII_EURUSD_M1201508/DAT_ASCII_EURUSD_M1_201508.csv'
+	), rewards), processor, investor, celebrator, capacitor)
+	.run()
+})
 .done();
