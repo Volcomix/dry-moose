0d0ac94e047d634d91d4585842309f01108d71ea
Volcomix <volcomix@gmail.com>
2015-10-03 13:51:20 +0200

    Format dateTime axis

--- src/app/www/Monitoring.ts
+++ src/app/www/Monitoring.ts
@@ -12,6 +12,16 @@ var y = d3.scale.linear()
 
 var xAxis = d3.svg.axis()
     .scale(x)
+    .tickFormat(d3.time.format.multi([
+        [".%L", function(d) { return d.getMilliseconds(); }],
+        [":%S", function(d) { return d.getSeconds(); }],
+        ["%H:%M", function(d) { return d.getMinutes(); }],
+        ["%H:%M", function(d) { return d.getHours(); }],
+        ["%a %d", function(d) { return d.getDay() && d.getDate() != 1; }],
+        ["%b %d", function(d) { return d.getDate() != 1; }],
+        ["%B", function(d) { return d.getMonth(); }],
+        ["%Y", function() { return true; }]
+    ]))
     .orient('bottom')
     .tickSize(-height, 0);
 
