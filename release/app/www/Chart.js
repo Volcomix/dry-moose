4272a2aa67e5c4f3863066d0e2401d4b1e88c346
Volcomix <volcomix@gmail.com>
2015-10-17 18:51:14 +0200

    Create initial chart with ReactJS and D3

--- release/app/www/Chart.js
+++ release/app/www/Chart.js
@@ -4,17 +4,51 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+var d3 = require('d3');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Chart = (function (_super) {
     __extends(Chart, _super);
     function Chart() {
         _super.apply(this, arguments);
+        this.x = d3.time.scale();
+        this.y = d3.scale.linear();
+        this.xAxis = d3.svg.axis()
+            .scale(this.x)
+            .tickFormat(d3.time.format.multi([
+            ['.%L', function (d) { return d.getMilliseconds(); }],
+            [':%S', function (d) { return d.getSeconds(); }],
+            ['%H:%M', function (d) { return d.getMinutes(); }],
+            ['%H:%M', function (d) { return d.getHours(); }],
+            ['%a %d', function (d) { return d.getDay() && d.getDate() != 1; }],
+            ['%b %d', function (d) { return d.getDate() != 1; }],
+            ['%B', function (d) { return d.getMonth(); }],
+            ['%Y', function () { return true; }]
+        ]))
+            .orient('bottom');
+        this.yAxis = d3.svg.axis()
+            .scale(this.y)
+            .tickFormat(d3.format(',.5f'))
+            .orient('right');
     }
     Chart.prototype.render = function () {
-        return React.createElement("div", null, "Hello ", this.props.name);
+        var _this = this;
+        var _a = this.props, width = _a.width, height = _a.height, marginTop = _a.marginTop, marginRight = _a.marginRight, marginBottom = _a.marginBottom, marginLeft = _a.marginLeft;
+        this.x.range([0, width]);
+        this.y.range([height, 0]);
+        this.xAxis.tickSize(-height, 0);
+        this.yAxis.tickSize(-width, 0);
+        return (React.createElement("svg", {"width": width + marginLeft + marginRight, "height": height + marginTop + marginBottom}, React.createElement("g", {"transform": 'translate(' + marginLeft + ', ' + marginTop + ')'}, React.createElement("g", {"className": "x axis", "transform": 'translate(0, ' + height + ')', "ref": function (ref) { return _this.xAxis(d3.select(ref)); }}), React.createElement("g", {"className": "y axis", "transform": 'translate(' + width + ', 0)', "ref": function (ref) { return _this.yAxis(d3.select(ref)); }}))));
     };
     return Chart;
 })(React.Component);
-var x = React.createElement(Chart, {"name": "World"});
-ReactDOM.render(x, document.getElementById('example'));
+var Chart;
+(function (Chart) {
+    Chart.defaultProps = {
+        marginTop: 20,
+        marginRight: 50,
+        marginBottom: 30,
+        marginLeft: 20
+    };
+})(Chart || (Chart = {}));
+ReactDOM.render(React.createElement(Chart, {"width": 800, "height": 600}), document.getElementById('chart'));
--- src/app/www/Chart.tsx
+++ src/app/www/Chart.tsx
@@ -1,14 +1,78 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
+import d3 = require('d3');
 import React = require('react');
 import ReactDOM = require('react-dom');
 
-class Chart extends React.Component<{ name: string }, { }> {
+class Chart extends React.Component<Chart.Props, Chart.State> {
+	
+	x = d3.time.scale<number, Date>();
+	y = d3.scale.linear();
+	
+	xAxis = d3.svg.axis()
+		.scale(this.x)
+		.tickFormat(d3.time.format.multi([
+			['.%L', function(d) { return d.getMilliseconds(); }],
+			[':%S', function(d) { return d.getSeconds(); }],
+			['%H:%M', function(d) { return d.getMinutes(); }],
+			['%H:%M', function(d) { return d.getHours(); }],
+			['%a %d', function(d) { return d.getDay() && d.getDate() != 1; }],
+			['%b %d', function(d) { return d.getDate() != 1; }],
+			['%B', function(d) { return d.getMonth(); }],
+			['%Y', function() { return true; }]
+		]))
+		.orient('bottom');
+		
+	yAxis = d3.svg.axis()
+		.scale(this.y)
+		.tickFormat(d3.format(',.5f'))
+		.orient('right');
+	
 	render() {
-		return <div>Hello {this.props.name}</div>;
+		var {width, height, marginTop, marginRight, marginBottom, marginLeft} = this.props;
+		
+		this.x.range([0, width]);
+        this.y.range([height, 0]);
+        
+        this.xAxis.tickSize(-height, 0);
+        this.yAxis.tickSize(-width, 0);
+		
+		return (
+			<svg
+				width={width + marginLeft + marginRight}
+				height={height + marginTop + marginBottom}>
+				<g transform={'translate(' + marginLeft + ', ' + marginTop + ')'}>
+					<g
+						className="x axis" transform={'translate(0, ' + height + ')'}
+						ref={(ref: any) => this.xAxis(d3.select(ref))} />
+					<g
+						className="y axis" transform={'translate(' + width + ', 0)'}
+						ref={(ref: any) => this.yAxis(d3.select(ref))} />
+				</g>
+			</svg>
+		);
 	}
 }
 
-var x = <Chart name="World" />;
+module Chart {
+	export interface Props {
+		width: number;
+		height: number;
+		marginTop?: number;
+		marginRight?: number;
+		marginBottom?: number;
+		marginLeft?: number;
+	}
 
-ReactDOM.render(x, document.getElementById('example'));
+	export var defaultProps = {
+		marginTop: 20,
+		marginRight: 50,
+		marginBottom: 30,
+		marginLeft: 20
+	}
+  
+	export interface State {
+	}
+}
+
+ReactDOM.render(<Chart width={800} height={600} />, document.getElementById('chart'));
