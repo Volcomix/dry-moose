dfa6dac4524ed02fb0463ace985d0e3fef4c868d
Volcomix <volcomix@gmail.com>
2015-11-09 19:42:18 +0100

    Cleanup index.tsx

--- release/app/www/Index.js
+++ release/app/www/Index.js
@@ -3,21 +3,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var WindowActions = require('./actions/WindowActions');
 var Chart = require('./components/Chart');
-var asQuote = function (dateTime, close) { return ({
-    dateTime: dateTime,
-    open: undefined,
-    high: undefined,
-    low: undefined,
-    close: close,
-    volume: undefined,
-    rewards: undefined
-}); };
-var data = [
-    asQuote(new Date('2015-10-20T10:00:00Z'), 1.125),
-    asQuote(new Date('2015-10-20T11:00:00Z'), 1.25),
-    asQuote(new Date('2015-10-20T12:00:00Z'), 1.20),
-    asQuote(new Date('2015-10-20T13:00:00Z'), 1.345)
-];
 var container = document.getElementById('chart');
 function resizeContainer() {
     WindowActions.resize(container.offsetWidth, container.offsetHeight);
--- src/app/www/Index.tsx
+++ src/app/www/Index.tsx
@@ -3,28 +3,9 @@
 import React = require('react');
 import ReactDOM = require('react-dom');
 
-import Quote = require('../../documents/Quote');
-
 import WindowActions = require('./actions/WindowActions');
 
 import Chart = require('./components/Chart');
-
-var asQuote = (dateTime: Date, close: number): Quote => ({
-	dateTime: dateTime,
-	open: undefined,
-	high: undefined,
-	low: undefined,
-	close: close,
-	volume: undefined,
-	rewards: undefined
-})
-
-var data = [
-	asQuote(new Date('2015-10-20T10:00:00Z'), 1.125),
-	asQuote(new Date('2015-10-20T11:00:00Z'), 1.25),
-	asQuote(new Date('2015-10-20T12:00:00Z'), 1.20),
-	asQuote(new Date('2015-10-20T13:00:00Z'), 1.345)
-];
 
 var container = document.getElementById('chart');
 
