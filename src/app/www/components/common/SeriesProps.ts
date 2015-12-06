fa238c90717fd9e5c98f5519fc553638125baa54
Volcomix <volcomix@gmail.com>
2015-12-06 10:09:37 +0100

    Add common SeriesProps class

---
+++ src/app/www/components/common/SeriesProps.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../../../../typings/tsd.d.ts" />
+
+import d3 = require('d3');
+
+interface SeriesProps {
+	xScale: d3.time.Scale<Date, number>;
+	yScale: d3.scale.Linear<number, number>;
+	clipPath: string;
+}
+
+export = SeriesProps;
