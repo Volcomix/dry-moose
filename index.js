3d8887a93c1c86844f7ffb104a4544b0422c2918
Volcomix <volcomix@gmail.com>
2017-10-22 16:20:12 +0200

    Refactor in class

--- index.js
+++ index.js
@@ -20,146 +20,146 @@ const urlActivity = `${urlApi}/trade-real/instruments/?InstrumentDataFilters`
 const urlPrivate = `${urlApi}/trade-real/instruments/private?InstrumentDataFilters`
 const urlInsights = `${urlApi}/insights/insights/uniques`
 
-let db
+class DryMoose {
+  async run() {
+    try {
+      const browser = await puppeteer.launch({ appMode: true, ...chromeOptions })
+      this.db = await MongoClient.connect(mongoUri)
+      await this.db.collection('executions').insertOne({
+        date: new Date(),
+        event: 'start',
+      })
+      const page = await browser.newPage()
+      page.on('response', this.receiveResponse.bind(this))
+      await page.goto(urlBase)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-async function main() {
-  try {
-    const browser = await puppeteer.launch({ appMode: true, ...chromeOptions })
+  async receiveResponse(response) {
+    const request = response.request()
+    if (request.url.startsWith(urlClosingPrices)) {
+      await this.receiveClosingPrices(await response.json())
+    } else if (request.url.startsWith(urlDisplayDatas)) {
+      await this.receiveDisplayDatas(await response.json())
+    } else if (request.url.startsWith(urlInstruments)) {
+      await this.receiveInstruments(await response.json())
+    } else if (request.url.startsWith(urlGroups)) {
+      await this.receiveGroups(await response.json())
+    } else if (request.url.startsWith(urlActivity)) {
+      await this.receiveActivity(await response.json())
+    } else if (request.url.startsWith(urlPrivate)) {
+      await this.receivePrivateInstruments(await response.json())
+    } else if (request.url.startsWith(urlInsights)) {
+      await this.receiveInsights(await response.json())
+    }
+  }
 
-    db = await MongoClient.connect(mongoUri)
-    await db.collection('executions').insertOne({
-      date: new Date(),
-      event: 'start',
-    })
+  async receiveClosingPrices(closingPrices) {
+    await this.db.collection('closingPrices')
+      .insertMany(closingPrices.map(
+        closingPrice => ({
+          date: new Date(),
+          ...closingPrice,
+        })))
+  }
 
-    const page = await browser.newPage()
-    page.on('response', async response => {
-      const request = response.request()
-      if (request.url.startsWith(urlClosingPrices)) {
-        await receiveClosingPrices(await response.json())
-      } else if (request.url.startsWith(urlDisplayDatas)) {
-        await receiveDisplayDatas(await response.json())
-      } else if (request.url.startsWith(urlInstruments)) {
-        await receiveInstruments(await response.json())
-      } else if (request.url.startsWith(urlGroups)) {
-        await receiveGroups(await response.json())
-      } else if (request.url.startsWith(urlActivity)) {
-        await receiveActivity(await response.json())
-      } else if (request.url.startsWith(urlPrivate)) {
-        await receivePrivateInstruments(await response.json())
-      } else if (request.url.startsWith(urlInsights)) {
-        await receiveInsights(await response.json())
-      }
-    })
-    await page.goto(urlBase)
-  } catch (error) {
-    console.error(error)
+  async receiveDisplayDatas(displayDatas) {
+    await this.db.collection('displayDatas')
+      .insertMany(displayDatas.InstrumentDisplayDatas.map(
+        instrument => ({
+          date: new Date(),
+          ...instrument,
+        })
+      ))
+  }
+
+  async receiveInstruments(instruments) {
+    await this.db.collection('instruments')
+      .insertMany(instruments.Instruments.map(
+        instrument => ({
+          date: new Date(),
+          ...instrument,
+        })
+      ))
+  }
+
+  async receiveGroups(groups) {
+    await this.receiveInstrumentTypes(groups.InstrumentTypes)
+    await this.receiveExchangeInfo(groups.ExchangeInfo)
+    await this.receiveStocksIndustries(groups.StocksIndustries)
+  }
+
+  async receiveInstrumentTypes(instrumentTypes) {
+    await this.db.collection('instrumentTypes')
+      .insertMany(instrumentTypes.map(
+        instrumentType => ({
+          date: new Date(),
+          ...instrumentType,
+        })
+      ))
+  }
+
+  async receiveExchangeInfo(exchangeInfo) {
+    await this.db.collection('exchangeInfo')
+      .insertMany(exchangeInfo.map(
+        exchange => ({
+          date: new Date(),
+          ...exchange,
+        })
+      ))
+  }
+
+  async receiveStocksIndustries(stocksIndustries) {
+    await this.db.collection('stocksIndustries')
+      .insertMany(stocksIndustries.map(
+        industry => ({
+          date: new Date(),
+          ...industry,
+        })
+      ))
+  }
+
+  async receiveActivity(activity) {
+    await this.receiveActivityStates(activity.InstrumentsToActivityState)
+    await this.receiveRates(activity.Rates)
+  }
+
+  async receiveActivityStates(activityStates) {
+    await this.db.collection('activityStates')
+      .insertMany(Object.keys(activityStates).map(
+        InstrumentId => ({
+          date: new Date(),
+          InstrumentId,
+          ActivityState: activityStates[InstrumentId],
+        })
+      ))
+  }
+
+  async receiveRates(rates) {
+    await this.db.collection('rates').insertMany(rates)
+  }
+
+  async receivePrivateInstruments(privateInstruments) {
+    await this.db.collection('privateInstruments')
+      .insertMany(privateInstruments.PrivateInstruments.map(
+        privateInstrument => ({
+          date: new Date(),
+          ...privateInstrument,
+        })
+      ))
+  }
+
+  async receiveInsights(insights) {
+    await this.db.collection('insights')
+      .insertMany(insights.map(
+        insight => ({
+          date: new Date(),
+          ...insight,
+        })
+      ))
   }
 }
 
-async function receiveClosingPrices(closingPrices) {
-  await db.collection('closingPrices')
-    .insertMany(closingPrices.map(
-      closingPrice => ({
-        date: new Date(),
-        ...closingPrice,
-      })))
-}
-
-async function receiveDisplayDatas(displayDatas) {
-  await db.collection('displayDatas')
-    .insertMany(displayDatas.InstrumentDisplayDatas.map(
-      instrument => ({
-        date: new Date(),
-        ...instrument,
-      })
-    ))
-}
-
-async function receiveInstruments(instruments) {
-  await db.collection('instruments')
-    .insertMany(instruments.Instruments.map(
-      instrument => ({
-        date: new Date(),
-        ...instrument,
-      })
-    ))
-}
-
-async function receiveGroups(groups) {
-  await receiveInstrumentTypes(groups.InstrumentTypes)
-  await receiveExchangeInfo(groups.ExchangeInfo)
-  await receiveStocksIndustries(groups.StocksIndustries)
-}
-
-async function receiveInstrumentTypes(instrumentTypes) {
-  await db.collection('instrumentTypes')
-    .insertMany(instrumentTypes.map(
-      instrumentType => ({
-        date: new Date(),
-        ...instrumentType,
-      })
-    ))
-}
-
-async function receiveExchangeInfo(exchangeInfo) {
-  await db.collection('exchangeInfo')
-    .insertMany(exchangeInfo.map(
-      exchange => ({
-        date: new Date(),
-        ...exchange,
-      })
-    ))
-}
-
-async function receiveStocksIndustries(stocksIndustries) {
-  await db.collection('stocksIndustries')
-    .insertMany(stocksIndustries.map(
-      industry => ({
-        date: new Date(),
-        ...industry,
-      })
-    ))
-}
-
-async function receiveActivity(activity) {
-  await receiveActivityStates(activity.InstrumentsToActivityState)
-  await receiveRates(activity.Rates)
-}
-
-async function receiveActivityStates(activityStates) {
-  await db.collection('activityStates')
-    .insertMany(Object.keys(activityStates).map(
-      InstrumentId => ({
-        date: new Date(),
-        InstrumentId,
-        ActivityState: activityStates[InstrumentId],
-      })
-    ))
-}
-
-async function receiveRates(rates) {
-  await db.collection('rates').insertMany(rates)
-}
-
-async function receivePrivateInstruments(privateInstruments) {
-  await db.collection('privateInstruments')
-    .insertMany(privateInstruments.PrivateInstruments.map(
-      privateInstrument => ({
-        date: new Date(),
-        ...privateInstrument,
-      })
-    ))
-}
-
-async function receiveInsights(insights) {
-  await db.collection('insights')
-    .insertMany(insights.map(
-      insight => ({
-        date: new Date(),
-        ...insight,
-      })
-    ))
-}
-
-main()
+new DryMoose().run()
