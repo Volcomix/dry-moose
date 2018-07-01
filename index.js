bfdc6da57a44a7f9cf2f9772fc39f06d04ec5303
Volcomix <volcomix@gmail.com>
2018-07-01 23:08:09 +0200

    Init market scan

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Chrome = require('./lib/chrome')
 const Login = require('./lib/login')
 const DemoMode = require('./lib/demo-mode')
+const Market = require('./lib/market')
 
 const chromePath = 'google-chrome-unstable'
 const demoMode = true
@@ -10,6 +11,7 @@ class DryMoose {
     const page = await new Chrome().launch(chromePath)
     await new Login(page).wait()
     await new DemoMode(page).set(demoMode)
+    await new Market(page).scan()
   }
 }
 
