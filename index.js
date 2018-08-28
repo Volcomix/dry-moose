9c2ce2cae7aea3adf8d6d7b04ebf7724117160f1
Volcomix <volcomix@gmail.com>
2018-08-28 20:12:18 +0200

    Start eToro from SocketIO

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 const Browser = require('./lib/browser')
+const EToro = require('./lib/etoro')
 
 server.listen(4000)
 
@@ -10,4 +11,9 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-new Browser(io)
+const browser = new Browser(io)
+new EToro(browser, io)
+
+process.on('unhandledRejection', error => {
+  throw error
+})
