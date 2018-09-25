e0bd869c5ad65e2f7690be5df994f72b7d808d5f
Volcomix <volcomix@gmail.com>
2018-09-25 20:15:28 +0200

    Open ProCharts

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Browser = require('./lib/browser')
 const EToro = require('./lib/etoro')
 const Market = require('./lib/market')
 const Filter = require('./lib/filter')
+const ProCharts = require('./lib/procharts')
 
 server.listen(4000)
 
@@ -17,6 +18,7 @@ const browser = new Browser(io)
 const eToro = new EToro(browser, io)
 const market = new Market(eToro, io)
 new Filter(market, io)
+new ProCharts(browser, eToro, io)
 
 process.on('unhandledRejection', error => {
   throw error
