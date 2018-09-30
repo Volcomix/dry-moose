fed47cf4ccb65523057902e870ea8fd12dd9e330
Volcomix <volcomix@gmail.com>
2018-09-30 12:39:52 +0200

    Init instruments charts loading

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const app = require('express')()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
-const Browser = require('./lib/browser')
-const EToro = require('./lib/etoro')
-const Market = require('./lib/market')
-const Filter = require('./lib/filter')
-const ProCharts = require('./lib/procharts')
+const Browser = require('./lib/Browser')
+const EToro = require('./lib/EToro')
+const Market = require('./lib/Market')
+const Filter = require('./lib/Filter')
+const ProCharts = require('./lib/ProCharts')
+const Chart = require('./lib/Chart')
 
 server.listen(4000)
 
@@ -17,8 +18,9 @@ app.get('/', (req, res) => {
 const browser = new Browser(io)
 const eToro = new EToro(browser, io)
 const market = new Market(eToro, io)
-new Filter(market, io)
-new ProCharts(browser, eToro, io)
+const filter = new Filter(market, io)
+const proCharts = new ProCharts(browser, eToro, io)
+new Chart(proCharts, filter, io)
 
 process.on('unhandledRejection', error => {
   throw error
